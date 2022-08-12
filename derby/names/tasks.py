407b7337c834fb1afdc219894ff66752ca345b8a
Brian Dunnette <dunn0172@umn.edu>
2022-08-12 15:38:16 -0500

    switching to model with custom tokenizer

--- derby/names/tasks.py
+++ derby/names/tasks.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from aitextgen import aitextgen
 from django.conf import settings
 from huey import crontab
@@ -8,28 +11,26 @@ from derby.names.models import Name
 logger = settings.LOGGER
 
 
-@db_periodic_task(
-    crontab(
-        day=settings.GENERATE_NAMES_DAY,
-        hour=settings.GENERATE_NAMES_HOUR,
-        minute=settings.GENERATE_NAMES_MINUTE,
-    ),
-    retries=3,
-)
-def generate_names():
+@db_periodic_task(crontab(minute="*/1"))
+def generate_name(max_length=64, prompt=None, temperature=1.0):
     try:
         model_dir = settings.MODEL_DIR
-        logger.info(f"Loading model from {model_dir.absolute()}")
-        ai = aitextgen(model_folder=str(model_dir))
+        logger.debug(f"Loading model from {model_dir.absolute()}")
+        ai = aitextgen(
+            model_folder=str(model_dir),
+            tokenizer_file=str(model_dir / "aitextgen.tokenizer.json"),
+        )
+        if prompt is None:
+            prompt = random.choice(string.ascii_uppercase)
+        logger.debug("Generating names")
+        generated = ai.generate_one(
+            prompt=prompt, temperature=temperature, max_length=max_length
+        ).strip()
+        print(generated)
 
-        logger.info("Generating names")
-        generated = ai.generate_one()
-        names = [Name(name=name.strip()) for name in str(generated).split("\n")]
-        logger.info(f"Generated names: {names}")
-
-        logger.info("Saving names")
-        Name.objects.bulk_create(names, ignore_conflicts=True)
-        logger.info("Done")
+        logger.debug("Saving names")
+        Name.objects.update_or_create(name=generated)
+        logger.debug("Done")
     except Exception as error:
         logger.error(f"Error generating names: {error}")
         raise Exception(error)
